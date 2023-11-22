8fa950f2faaf397a47a7c9f8f9449aee535c2a36
Clinton Raj <clintonraj007@outlook.com>
2023-11-22 15:04:54 +0530

    first commit

---
+++ src/com/java/Check_odd_or_even_number.java
@@ -0,0 +1,16 @@
+package com.java;
+
+public class Check_odd_or_even_number {
+
+	public static void main(String[] args) {
+		int a=11;
+		if(a%2==0) {
+			System.out.println(a+" is Even Number");
+		}
+		else {
+			System.out.println(a+" is Odd Number");
+		}
+
+	}
+
+}
