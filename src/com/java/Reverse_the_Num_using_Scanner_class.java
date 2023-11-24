b165bb1cce763204b36cb557b4cdb1c4fc87b847
Clinton Raj <clintonraj007@outlook.com>
2023-11-24 19:31:03 +0530

    24_Nov

---
+++ src/com/java/Reverse_the_Num_using_Scanner_class.java
@@ -0,0 +1,29 @@
+package com.java;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Reverse_the_Num_using_Scanner_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Number: ");
+
+		try {
+			int rev = sc.nextInt();
+			int remainter = 0;
+			while (rev > 0) {
+				int n = (rev % 10);
+				remainter = (remainter * 10) + n;
+				rev /= 10;
+			}
+			System.out.println(remainter);
+		} catch (InputMismatchException e) {
+			System.out.println("Invalid input. Please enter a valid number.");
+		}
+
+		sc.close();
+	}
+
+}
